0092a91afd8ad260c49f826f6fe604bd0573b54b
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-18 22:30:25 -0600

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,6 @@
+// Configuración general de la aplicación
+export const appConfig = {
+  name: "PinolApp Nica Delivery",
+  version: "1.0.0",
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
+};
